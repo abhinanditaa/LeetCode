67a892fdbdf7fa677f87a45c87f3516ed3751555
Abhinandita <abhinanditanandi@gmail.com>
2026-09-24 10:03:18 +0530

    Time: 2 ms (66.36%) | Memory: 43 MB (71.02%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            result ^= s.charAt(i);
+            result ^= t.charAt(i);
+        }
+
+        result ^= t.charAt(t.length() - 1);
+
+        return result;
+    }
+}
