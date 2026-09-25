9a4f4cd476be323465ee80ef57b580459c0f42b5
Abhinandita <abhinanditanandi@gmail.com>
2026-09-25 11:30:44 +0530

    Time: 1506 ms (30.29%) | Memory: 60.7 MB (85.70%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+class Solution {
+    private int[] nums;
+    private Random random;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.random = new Random();
+    }
+
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                count++;
+
+                // Choose current index with probability 1/count
+                if (random.nextInt(count) == 0) {
+                    result = i;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
