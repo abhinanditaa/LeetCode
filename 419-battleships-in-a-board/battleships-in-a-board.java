4eec5e3efd63c8d33e0117211126892b194c0a17
Abhinandita <abhinanditanandi@gmail.com>
2026-09-26 12:17:15 +0530

    Time: 1 ms (98.54%) | Memory: 45.6 MB (52.07%) - LeetSync

---
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (board[i][j] != 'X') {
+                    continue;
+                }
+
+                // If connected to a ship above or to the left,
+                // this is not the starting cell.
+                if (i > 0 && board[i - 1][j] == 'X') {
+                    continue;
+                }
+
+                if (j > 0 && board[i][j - 1] == 'X') {
+                    continue;
+                }
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
